80fcc5f4a98f495142803015477add19d694382c
Muguthangit <muguthan.masti@gmail.com>
2015-11-04 15:58:06 +0530

    Second commit with comments

--- pdf_page_numbers.py
+++ pdf_page_numbers.py
@@ -1,16 +1,15 @@
-import pypdftk
+import pypdftk #install pypdftk with 'pip install pypdftk'
 import os
 import re
+import csv
 
-path = '/home/qruizer/Downloads/pdf/*.pdf'
-file_path = '/home/qruizer/Downloads/pdf/'
-dirList=os.listdir(file_path)
-file_write = open('/home/qruizer/open.csv', 'w')
+file_path = 'path/to/file' #path to the file location	
+dirList=os.listdir(file_path) #list all the files in the directories
+file_write = open('path/to/export/as/csv', 'w') #writing no.of pages into a csv file
 for fname in dirList:
-	data_find = fname , pypdftk.get_num_pages(file_path + fname)
-	print data_find
+	data_find = fname , pypdftk.get_num_pages(file_path + fname)  # giving file path with the name of the file
+	print data_find # test with printing the data
 	file_write.write(repr(data_find)) 
 	
-
 file_write.close()
 
