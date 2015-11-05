3554766ec461fc3a50df339830325a50ef272e27
Muguthangit <muguthan.masti@gmail.com>
2015-11-05 17:20:10 +0530

    New line added

--- pdf_page_numbers.py
+++ pdf_page_numbers.py
@@ -3,13 +3,14 @@ import os
 import re
 import csv
 
-file_path = 'path/to/file' #path to the file location	
+file_path = '/home/qruizer/Downloads/pdf/' #path to the file location	
 dirList=os.listdir(file_path) #list all the files in the directories
-file_write = open('path/to/export/as/csv', 'w') #writing no.of pages into a csv file
+file_write = open('/home/qruizer/Downloads/open.csv', 'w') #writing no.of pages into a csv file
 for fname in dirList:
-	data_find = fname , pypdftk.get_num_pages(file_path + fname)  # giving file path with the name of the file
-	print data_find # test with printing the data
-	file_write.write(repr(data_find)) 
+	data_find = fname , str(pypdftk.get_num_pages(file_path + fname))  # giving file path with the name of the file
+	new_data = data_find # test with printing the data
+	print new_data	
+	file_write.write(str(new_data) + '\n')
 	
 file_write.close()
 
